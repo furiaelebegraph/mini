669f4410b0b2389cd36d411e8ccb14dbc4c23a75
furiaelebegraph <webmaster@elebegraph.com>
2018-02-16 18:11:34 -0600

    'termine_baby_casi_casi_termino_primeros_pasos'

--- resources/views/baby.blade.php
+++ resources/views/baby.blade.php
@@ -12,28 +12,43 @@
 						<img class="logo_baby"  src="{{ asset('img/logo_baby.svg') }}" alt="">
 					</div>
 					<div class="col-12 fondo_hojas">
-						<div class="row">
-							<div class="col-12 col-md-6 order-2 order-sm-1 ">
+						<div class="row justify-content-center">
+							<div class="col-12 col-md-5 col-lg-6 order-2 order-md-1 ">
 								<div class="zapatito_baby_1">
 									<img src="{{ asset('img//baby/zapatito_baby_1.png') }}" alt="">
 								</div>
 							</div>
-							<div class="col-12 col-md-6 order-1 order-sm-2 ">
+							<div class="col-12 col-md-5 col-lg-6 order-1 order-md-2 ">
 								<div class="wrap_texto_1">
 									<div class="titulo_baby_1">
 										<p data-texto='HERMOSOS'>HERMOSOS</p>
 										<p data-texto='MODA'><span data-texto='Y A LA'>Y A LA </span>  MODA</p>
 									</div>
-									<div class="texto_baby_1">
+									<div class="texto_baby_1 escritorio">
 										<div class="texto_baby">
 											<p>
-												Para los más pequeños
+												Para los más pequeños 
 												de la casa lo escencial es
-												<span> la comodidad y permitir el 
+												<span> la comodidad y  permitir el 
 												desarrollo 
 												de su pie,</span>
 												pero tambíen 
 												verse hermosos
+												y a la moda.
+
+											</p>
+										</div>
+									</div>
+									<div class="texto_baby_1 mobiles">
+										<div class="texto_baby">
+											<p>
+												Para los más pequeños <br>
+												de la casa lo escencial es<br>
+												<span> la comodidad y  <br>permitir el 
+												desarrollo <br>
+												de su pie,</span><br>
+												pero tambíen <br>
+												verse hermosos<br>
 												y a la moda.
 
 											</p>
@@ -47,11 +62,11 @@
 			</div>
 		</div>
 	</div>
-	<div class="row">
+	<div class="row m-t-70">
 		<div class="col-12">
 			<div class="centro_baby">
 				<div class="row align-items-center">
-					<div class="col-12 col-sm-6">
+					<div class="col-6">
 						<div class="row">
 							<div class="col-12 col-sm-6">
 								<div class="titulo_baby_2">
@@ -63,10 +78,11 @@
 							<div class="col-12 col-sm-6">
 								<div class="texto_baby_2">
 									<p>
-										Utilizamos pieles tamboreadas muy suaves ideales para estar en contacto con el recién llegado.
+										<span>Utilizamos pieles </span>
+										tamboreadas muy suaves ideales para estar en contacto con el recién llegado.
 									</p>
-									<p>
-										Forro y plantilla 
+									<p class='m-t-10'>
+										<span>Forro y plantilla </span>
 			natural de cerdo que permiten la transpiración del pie y disminuyen la humedad.
 									</p>
 								</div>
@@ -74,29 +90,36 @@
 						</div>
 					</div>
 
-					<div class="col-12 col-sm-6">
-						<div class="fondo_pescaito">
+					<div class="col-6">
+						<div class="fondo_pescaito escritorio">
 							<img src="{{ asset('img/baby/zapatito_baby_2.png') }}" alt="">
+						</div>
+						<div class="fondo_pescaito mobiles">
+							<img src="{{ asset('img/baby/zapatito_baby_2_1.png') }}" alt="">
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="row">
+	<div class="row m-t-200-n">
 		<div class="col-12">
 			<div class="centro_baby">
 				<div class="row align-items-center">
 					<div class="col-12 col-sm-6">
-						<div class="fondo_solishito">
+						<div class="fondo_solishito escritorio">
 							<img src="{{ asset('img/baby/zapatito_baby_3.png') }}" alt="">
 						</div>
+
+						<div class="fondo_solishito mobiles ">
+							<img src="{{ asset('img/baby/zapatito_baby_3_1.png') }}" alt="">
+						</div>
 					</div>
-					<div class="col-12 col-sm-6">
+					<div class="col-12 col-sm-6 m-t-n-300">
 						<div class="texto_baby_3">
 							<p>
 								Materiales como
-								piel y el TR ultraligero fueron seleccionados para asegurar una suela super cómoda.
+								<span>piel y el TR ultraligero</span> fueron seleccionados para asegurar una suela super cómoda.
 							</p>
 						</div>
 					</div>
@@ -104,18 +127,88 @@
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-12 col-sm-6">
+	<div class="row align-items-center m-t-100-n">
+		<div class="col-12 col-sm-6 order-2 order-sm-1 zapato_gigante_baby">
 			<img src="{{ asset('img/baby/zapatito_baby_4.png') }}" alt="">
 		</div>
-		<div class="col-12 col-sm-6">
-			<div class="">
+		<div class="col-12 col-sm-6 order-1 order-sm-2 alienado_centro">
+			<div class="titulo_baby_4">
 				<p>DISEÑO</p>
-				<p>ESPECTACULAR</p>
+				<p><span>ESPECTACULAR</span> </p>
+			</div>
+			<div class="texto_baby_4 mobiles">
+				<p>
+					Que tu <span>bebé luzca <br> espectacular</span> también <br>
+					es muy importante para<br> nosotros, <span>buscamos <br>accesorios ligeros <br>
+					discretos</span> y <br>
+					adecuados para <br>
+					no lastimar <br>
+					la piel. <br>
+				</p>
+			</div>
+			<div class="texto_baby_4 escritorio">
+				<p>
+					Que tu <span>bebé luzca espectacular</span> también 
+					es muy importante para nosotros, <span>buscamos accesorios ligeros 
+					discretos</span> y 
+					adecuados para 
+					no lastimar 
+					la piel. 
+				</p>
 			</div>
 		</div>
 	</div>
 </div>
-
+<div class="row m-t-80 align-items-center">
+	<div class="col-12 col-sm-7">
+		<div class="wrap_cat_baby">
+			<div class="texto_cat_baby alienado_derecha">
+				<h1>
+					Categorías <br>
+					<span>
+						KIDS
+					</span>
+					
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-5">
+		<div class="btn_catalogo_baby">
+			CATÁLOGO
+		</div>
+	</div>
+	<div class="col-12">
+		<div class="centro_baby">
+			<div class="row justify-content-center m-b-50">
+				<div class="col-12 col-sm-8">
+					<div class="row justify-content-between">
+						<div class="col-11 col-sm-5">
+							<div class="wrap_catalogo_baby">
+								<a href="#">
+									<img class='imagen_catalogo_baby' src="{{ asset('img/kids/barquito.png') }}" alt="">
+								</a>
+							</div>
+							<div class="m-t-20 alienado_centro">
+								<a class='titulo_baby_cata' href="#">Barquito</a>
+							</div>
+						</div>
+						<div class="col-11 col-sm-5">
+							<div class="wrap_catalogo_baby">
+								<a href="#">
+									<img class='imagen_catalogo_kids' src="{{ asset('img/kids/milan.png') }}" alt="">
+								</a>
+							</div>
+							<div class="m-t-20 alienado_centro">
+								<a class='titulo_baby_cata' href="#">Milán</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
 
 @endsection
--- resources/views/primeros.blade.php
+++ resources/views/primeros.blade.php
@@ -3,8 +3,230 @@
 @section('content')
 
 <div class="escondido">
+	<div class="row gusano_cosmico_pri">
+		<div class="col-12">
+			<div class="centro_primeros m-t-100">
+				<div class="row">
+					<div class="col-12">
+						<div class="logo_primeros">
+							<img src="{{ asset('img/logo_pasos.svg') }}" alt="">
+						</div>
+					</div>
+					<div class="col-12 flexeado">
+						<div class="escalera_1">
+							<img src="{{ asset('img/primeros/escalera_1.svg') }}" alt="">
+						</div>
+						<div class="wrap_texto_pasos_1">
+							<div class="row">
+								<div class="col-12 col-md-6">
+									<div class="texto_pasos_1">
+										<p>
+											Cuando tu bebé comienza a 
+											caminar se abre un mundo 
+											nuevo para explorar, en esta 
+											importante etapa necesita de 
+											un <span> calzado que le de 
+											conﬁanza</span> y a su vez 
+											ayude al <span> correcto 
+											desarrollo del pie.</span>
+										</p>
+									</div>
+								</div>
+								<div class="col-12 col-md-6">
+									<div class="titulo_pasos_1">
+										<p><span>UN </span>MUNDO</p>
+										<p>NUEVO PARA</p>
+										<p>EXPLORAR</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="row">
+							<div class="col-12 col-md-6">
+								<div class="wrap_nuvecita">
+									<div class="img_nubecita"> 
+										<img src="{{ asset('img/primeros/nubecita_pri.svg') }}" alt="">
+									</div>
+									<div class="titulo_pasos_2">
+										<p>SÚPER</p>
+										<p>cómodos</p>
+										<p>GRAN DISEÑO</p>
+										<p>Y RESISTENCIA</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-md-6">
+								<img src="{{ asset('img/primeros/zapato_1_primeros.png') }}" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="row">
 		<div class="col-12">
+			<div class="centro_primeros">
+				<div class="row align-items-center">
+					<div class="col-11 col-md-4">
+						<div class="texto_pasos_1">
+							<p>
+								En esta edad tus campeones 
+								tiene gran actividad, por eso 
+								seleccionamos <span>materiales 
+								de piel como napa y 
+								charoles</span> además de 
+								algunos sintéticos, 
+
+							</p>
+						</div>
+					</div>
+					<div class="col-11 col-md-5">
+						<img class='zapatito_primero_2' src="{{ asset('img/primeros/zapato_2_primeros.png') }}" alt="">
+					</div>
+				</div>
+			</div>
+		</div>	
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<div class="centro_primeros">
+				<div class="row align-items-end">
+					<div class="col-12 col-md-5">
+						<div class="wrap_zapatito_3">
+							<img class='zapatito_primeros_3' src="{{ asset('img/primeros/zapato_3_primeros.png') }}" alt="">
+						</div>
+					</div>
+					<div class="col-12 col-md-7">
+						<div class="wrap_todo_resiste">
+							<img src="{{ asset('img/primeros/todo_resiste_pri_12.svg') }}" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row m-t-60">
+		<div class="col-12">
+			<div class="centro_primeros">
+				<div class="row align-content-center">
+					<div class="col-12 col-sm-6 flexeado">
+						<div class="texto_primeros_3">
+							<p>
+								Nuestra experiencia en el calzado de 
+								bebés, nos ha llevado a implementar 
+								tecnología que brinde una total 
+								comodidad y seguridad:
+							</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="row">
+							<div class="col-10 flexeado">
+								<div class="icono_listado"><img src="{{ asset('img/primeros/iconito_1_miniburgujas.svg') }}" alt=""></div>
+								<p class="detalle_listado">
+									Suelas de TR de alta 
+									ﬂexibilidad que brindan 
+									confort.
+								</p>
+							</div>
+							<div class="col-10 flexeado">
+								<div class="icono_listado"><img src="{{ asset('img/primeros/iconito_2_miniburgujas.svg') }}" alt=""></div>
+								<p class="detalle_listado">
+									Punta de la suela 
+									elevada le da mayor 
+									resistencia a la abrasión.
+								</p>
+							</div>
+							<div class="col-10 flexeado">
+								<div class="icono_listado"><img src="{{ asset('img/primeros/iconito_3_miniburgujas.svg') }}" alt=""></div>
+								<p class="detalle_listado">
+									Soporte del talón dá 
+									mayor seguridad en sus 
+									primeros pasos.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<div class="centro_primeros">
+				<div class="row">
+					<div class="col-12 col-md-6">
+						<img src="{{ asset('img/primeros/zapato_4_primeros.png') }}" alt="">
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="texto_primeros_4">
+							<p>
+								A partir de esta etapa el
+								<span>zapato cuenta con arco 
+								almendra marcado</span>
+								que, junto al 
+								<span>
+									forro y la plantilla 
+									de piel de cerdo
+								</span>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row m-t-80 align-items-center">
+	<div class="col-12 col-sm-7">
+		<div class="wrap_cat_baby">
+			<div class="texto_cat_baby alienado_derecha">
+				<h1>
+					Categorías <br>
+					<span>
+						KIDS
+					</span>
+					
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-5">
+		<div class="btn_catalogo_baby">
+			CATÁLOGO
+		</div>
+	</div>
+	<div class="col-12">
+		<div class="centro_baby">
+			<div class="row justify-content-center m-b-50">
+				<div class="col-12 col-sm-8">
+					<div class="row justify-content-between">
+						<div class="col-11 col-sm-5">
+							<div class="wrap_catalogo_baby">
+								<a href="#">
+									<img class='imagen_catalogo_baby' src="{{ asset('img/kids/barquito.png') }}" alt="">
+								</a>
+							</div>
+							<div class="m-t-20 alienado_centro">
+								<a class='titulo_baby_cata' href="#">Barquito</a>
+							</div>
+						</div>
+						<div class="col-11 col-sm-5">
+							<div class="wrap_catalogo_baby">
+								<a href="#">
+									<img class='imagen_catalogo_kids' src="{{ asset('img/kids/milan.png') }}" alt="">
+								</a>
+							</div>
+							<div class="m-t-20 alienado_centro">
+								<a class='titulo_baby_cata' href="#">Milán</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 			
 		</div>
 	</div>
